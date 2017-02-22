701f2b211b1eeb0dc7d40a3c12b373000a835a80
Chengcheng <lcc199533@gmail.com>
2017-02-22 10:04:24 +0800

    Create Tagging class

---
+++ src/seedu/addressbook/data/tag/Tagging.java
@@ -0,0 +1,16 @@
+package seedu.addressbook.data.tag;
+import seedu.addressbook.data.person.Person;
+
+public class Tagging {
+	private enum TaggingType {ADD, DELETE};
+	
+	private Person _person;
+	private Tag _tag;
+	private TaggingType _taggingType;
+	
+	public Tagging(Person person, Tag tag, TaggingType taggingType){
+		_person = person;
+		_tag = tag;
+		_taggingType = taggingType;
+	}
+}
