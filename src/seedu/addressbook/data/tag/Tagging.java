27c7f8ce781f38cbddb3c36eade57a538361155e
Chengcheng <lcc199533@gmail.com>
2017-02-22 10:48:07 +0800

    Add to String method for tagging

--- src/seedu/addressbook/data/tag/Tagging.java
+++ src/seedu/addressbook/data/tag/Tagging.java
@@ -14,10 +14,46 @@ public class Tagging {
 	private Person _person;
 	private Tag _tag;
 	private TaggingType _taggingType;
+	
+	public static final String ADD_TAGGING_EXPRESSION = "+";
+	public static final String DELETE_TAGGING_EXPRESSION = "-";
 
 	public Tagging(Person person, Tag tag, TaggingType taggingType){
 		_person = person;
 		_tag = tag;
 		_taggingType = taggingType;
+	}
+
+	/**
+	 * @return the _person
+	 */
+	public Person get_person() {
+		return _person;
+	}
+
+	/**
+	 * @return the _tag
+	 */
+	public Tag get_tag() {
+		return _tag;
+	}
+
+	/**
+	 * @return the _taggingType
+	 */
+	public TaggingType get_taggingType() {
+		return _taggingType;
+	}
+	
+	private String getTypeExpression(){
+		if(this.get_taggingType().equals(TaggingType.ADD)){
+			return ADD_TAGGING_EXPRESSION;
+		}
+		return DELETE_TAGGING_EXPRESSION;
+	}
+	
+	public String toString(){
+		return this.getTypeExpression()+ " " + this.get_person().getName() + " " 
+				+ this.get_tag();
 	}
 }
