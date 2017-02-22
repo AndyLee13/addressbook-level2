67955a043891f3d7c6b2927f85c6e12d570d17e9
Chengcheng <lcc199533@gmail.com>
2017-02-22 08:46:51 +0800

    Add getPrintableString method

--- src/seedu/addressbook/data/person/Printable.java
+++ src/seedu/addressbook/data/person/Printable.java
@@ -3,5 +3,8 @@ package seedu.addressbook.data.person;
  * A read-only immutable interface for printables in the addressbook.
  */
 public interface Printable {
-
+	String toString();
+	default String getPrintableString(){
+		return this.toString();
+	}
 }
