cab5a454f432a5316e97c49adf11143db3ae8c9d
Chengcheng <lcc199533@gmail.com>
2017-02-23 15:42:31 +0800

    remove unnecessary imports

--- test/java/seedu/addressbook/data/person/NameTest.java
+++ test/java/seedu/addressbook/data/person/NameTest.java
@@ -1,9 +1,7 @@
 package seedu.addressbook.data.person;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
 
